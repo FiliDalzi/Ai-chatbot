3392999b9650570585697124b2505af672d1810d
FiliDalzi <fili.dalzi@gmail.com>
2026-02-18 19:51:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,12 +16,13 @@ const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+// 📁 Cartella pubblica
 const publicPath = join(__dirname, 'public');
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname));
-app.use(express.static(publicPath));
+app.use(express.static(publicPath)); // Serve index.html, video, css, ecc.
 
 const ai = new GoogleGenAI({
   apiKey: process.env.GOOGLE_API_KEY
@@ -74,7 +75,7 @@ app.post('/chat', async (req, res) => {
   }
 });
 
-// 👇 QUESTO APRE L’INDEX.HTML
+// 👇 Serve l'index.html principale
 app.get('/', (req, res) => {
   res.sendFile(join(publicPath, 'index.html'));
 });
